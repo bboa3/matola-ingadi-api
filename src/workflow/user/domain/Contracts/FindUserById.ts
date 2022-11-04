868f7aad4052741a650c897e224d3081b1be313e
Arlindo <arlindojosboa@gmail.com>
2022-11-05 00:16:13 +0200

    FEATURE: Get User

--- src/workflow/user/domain/Contracts/FindUserById.ts
+++ src/workflow/user/domain/Contracts/FindUserById.ts
@@ -1,4 +1,6 @@
+import { HttpErrorResponse } from '@core/infra/middleware/http_error_response'
 import { Id } from '@user/domain/requiredFields/id'
+import * as TE from 'fp-ts/lib/TaskEither'
 import { User } from 'ingadi'
 
 interface FindUserByIdProps {
@@ -6,3 +8,6 @@ interface FindUserByIdProps {
 }
 
 export type FindUserByIdDB = (data: FindUserByIdProps) => Promise<User>
+
+export type FindUserByIdService = (db: FindUserByIdDB) =>
+(data: FindUserByIdProps) => TE.TaskEither<HttpErrorResponse, User>
