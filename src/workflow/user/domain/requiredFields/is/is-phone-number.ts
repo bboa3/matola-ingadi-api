a7caaecfdd3323200c2d9bd4cce1b5a0bf7b1788
Arlindo <arlindojosboa@gmail.com>
2022-11-05 03:47:04 +0200

    IMPROVE: Number requiments

--- src/workflow/user/domain/requiredFields/is/is-phone-number.ts
+++ src/workflow/user/domain/requiredFields/is/is-phone-number.ts
@@ -1,7 +1,10 @@
 export const isPhoneNumber = (value: string) => {
   const regex = /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/
 
-  if (value.match(regex)) {
+  if (
+    value.match(regex) ||
+    (value.length >= 7 && value.length <= 14)
+  ) {
     return true
   }
 
